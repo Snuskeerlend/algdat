04cbee703b75826b6df65dd0fd4860ba1326eac5
Snuskeerlend <erlendrog@gmail.com>
2014-01-03 13:43:43 +0100

    vhKontroll

--- src/algdat/hjelpelasser/Tabell.java
+++ src/algdat/hjelpelasser/Tabell.java
@@ -177,5 +177,21 @@ public class Tabell
     return a;
   }
   
+  public static void vhKontroll(int tablengde, int v, int h)
+  {
+    if (v < 0)
+      throw new ArrayIndexOutOfBoundsException("v(" + v + ") < 0");
+
+    if (h >= tablengde)
+      throw new ArrayIndexOutOfBoundsException
+        ("h(" + h + ") >= tablengde(" + tablengde + ")");
+
+    if (v > h + 1)
+      throw new IllegalArgumentException
+        ("v = " + v + ", h = " + h);
+  }
+  
+  
+  
   
 }
